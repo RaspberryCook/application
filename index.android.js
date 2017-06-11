49ab5584fddfcbcff6be2cf2bebdd67843866ba3
Alexandre Rousseau <madeindjs@gmail.com>
2017-06-11 18:38:07 +0200

    Try to setup navigation

--- index.android.js
+++ index.android.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
+  Button,
   View,
   ActivityIndicator,
   ListView,
   Text
 } from 'react-native';
+import {
+  StackNavigator
+} from 'react-navigation';
 import Title from './components/Title';
 import Recipes from './components/Recipes';
 
-export default class raspberry_cook extends Component {
+
+
+
+
+
+export default class raspberry_cook extends React.Component {
+
+  navigation = "alex";
+
+  static navigationOptions = { title: 'Welcome', };
 
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={{flex: 1, paddingTop: 20}}>
         <Title name='Raspberry Cook' />
+
         <Recipes />
+        <Button title="Go to Jane's profile" onPress={() => navigate('Profile', { name: 'Jane' }) } />
       </View>
     );
   }
 }
 
+
+
+const App = StackNavigator({ Home: { screen: raspberry_cook },  });
 
 
 AppRegistry.registerComponent('raspberry_cook', () => raspberry_cook);
